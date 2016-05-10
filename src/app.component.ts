121c1918c1192ac594f65a38e355111511011f57
Paul Gschwendtner <paulgschwendtner@me.com>
2016-05-10 21:06:52 +0200

    update(): add license to plunker files

--- src/app.component.ts
+++ src/app.component.ts
@@ -11,3 +11,9 @@ import {MdButton} from '@angular2-material/button';
 export class AppComponent {}
 
 bootstrap(AppComponent);
+
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */
--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -40,3 +40,9 @@ _defaultPackages.forEach(function (item) {
 
 // Apply the new generated packages to the SystemJS configuration.
 System.config({ packages: _packageConfig });
+
+/*
+ Copyright 2016 Google Inc. All Rights Reserved.
+ Use of this source code is governed by an MIT-style license that
+ can be found in the LICENSE file at http://angular.io/license
+ */
